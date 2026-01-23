8c9ec48693189102c0c1a104c654f025f989e314
Leif Halvor <leif.halvor@arkitektum.no>
2026-01-23 14:02:22 +0100

    refactor: Reformatted with ruff

--- src/mvt_postgresql_ext/__init__.py
+++ src/mvt_postgresql_ext/__init__.py
@@ -18,27 +18,46 @@ class MVTPostgreSQLExtendedProvider(MVTPostgreSQLProvider):
     def __init__(self, provider_def: Dict):
         MVTPostgreSQLProvider.__init__(self, provider_def)
 
-        self._layer: str = provider_def.get('layer', self.table)
-        self._setup_caching(provider_def.get('cache'))
+        self._layer: str = provider_def.get("layer", self.table)
+        self._setup_caching(provider_def.get("cache"))
 
     def get_layer(self) -> str:
         return self._layer
 
-    def get_tiles(self, layer=None, tileset=None, z=None, y=None, x=None, format_=None) -> bytes:
+    def get_tiles(
+        self, layer=None, tileset=None, z=None, y=None, x=None, format_=None
+    ) -> bytes:
         z, y, x = map(int, [str(z), str(y), str(x)])
 
         [tileset_schema] = [
-            schema for schema in self.get_tiling_schemes()
+            schema
+            for schema in self.get_tiling_schemes()
             if tileset == schema.tileMatrixSet
         ]
 
         if not self.is_in_limits(tileset_schema, z, x, y):
             return bytes()
 
-        tile_path = str(Path(self._get_instance_path()).joinpath(f'{tileset}/{z}/{y}/{x}.pbf'))
+        tile_path = str(
+            Path(self._get_instance_path()).joinpath(f"{tileset}/{z}/{y}/{x}.pbf")
+        )
 
-        result = _get_tiles(str(layer), tileset_schema.tileMatrixSet, z, y, x, self.storage_crs, tileset_schema.crs, self._engine,
-                            self.table_model, self.geom, self.fields, self.get_envelope, tile_path, self._cache_options)
+        result = _get_tiles(
+            str(layer),
+            tileset_schema.tileMatrixSet,
+            z,
+            y,
+            x,
+            self.storage_crs,
+            tileset_schema.crs,
+            self._engine,
+            self.table_model,
+            self.geom,
+            self.fields,
+            self.get_envelope,
+            tile_path,
+            self._cache_options,
+        )
 
         return result
 
@@ -47,24 +66,40 @@ class MVTPostgreSQLExtendedProvider(MVTPostgreSQLProvider):
             self._cache_options = None
             return
 
-        base_path: str = cache_options['path']
+        base_path: str = cache_options["path"]
 
         self._cache_options = {
-            'base_path': Path(base_path),
-            'max_age_days': cache_options.get('max_age_days', 7)
+            "base_path": Path(base_path),
+            "max_age_days": cache_options.get("max_age_days", 7),
         }
 
     def _get_instance_path(self) -> str:
-        return f'{self.db_name}/{self.db_search_path[0]}/{self.table}'
+        return f"{self.db_name}/{self.db_search_path[0]}/{self.table}"
 
     def __repr__(self):
-        return f'<MVTPostgreSQLExtendedProvider> {self.data}'
+        return f"<MVTPostgreSQLExtendedProvider> {self.data}"
 
 
-_mem_cache = TTLCache(maxsize=640*1024, ttl=_MEM_CACHE_DAYS * 86400)
+_mem_cache = TTLCache(maxsize=640 * 1024, ttl=_MEM_CACHE_DAYS * 86400)
 
 
-@cached(cache=_mem_cache, key=lambda layer, tileset, z, y, x, storage_crs, tileset_schema_crs, engine, table_model, geom, fields, get_envelope_func, tile_path, cache_options: keys.hashkey(tile_path))
+@cached(
+    cache=_mem_cache,
+    key=lambda layer,
+    tileset,
+    z,
+    y,
+    x,
+    storage_crs,
+    tileset_schema_crs,
+    engine,
+    table_model,
+    geom,
+    fields,
+    get_envelope_func,
+    tile_path,
+    cache_options: keys.hashkey(tile_path),
+)
 def _get_tiles(
     layer: str,
     tileset: str,
@@ -79,15 +114,17 @@ def _get_tiles(
     fields: Dict,
     get_envelope_func: Callable[[int, int, int, str], Label],
     tile_path: str,
-    cache_options: Optional[Dict[str, Any]] = None
+    cache_options: Optional[Dict[str, Any]] = None,
 ) -> bytes:
     if cache_options:
-        base_path: Path = cache_options['base_path']
+        base_path: Path = cache_options["base_path"]
         tile_cache_path = base_path.joinpath(tile_path)
-        max_age = cache_options['max_age_days']
+        max_age = cache_options["max_age_days"]
 
-        if tile_cache_path.exists() and not _should_refresh_cache(tile_cache_path, max_age):
-            with open(tile_cache_path, 'rb') as file:
+        if tile_cache_path.exists() and not _should_refresh_cache(
+            tile_cache_path, max_age
+        ):
+            with open(tile_cache_path, "rb") as file:
                 return file.read()
 
     storage_srid = get_crs(storage_crs).to_string()
@@ -100,33 +137,25 @@ def _get_tiles(
         ST_Transform(envelope, storage_srid)  # type: ignore
     )
 
-    mvtgeom = (
-        ST_AsMVTGeom(
-            ST_Transform(ST_CurveToLine(geom_column), out_srid),
-            ST_Transform(envelope, out_srid))
-        .label('mvtgeom')
-    )
+    mvtgeom = ST_AsMVTGeom(
+        ST_Transform(ST_CurveToLine(geom_column), out_srid),
+        ST_Transform(envelope, out_srid),
+    ).label("mvtgeom")
 
-    mvtrow = (
-        select(mvtgeom, *fields.values())
-        .filter(geom_filter)
-        .cte('mvtrow')
-    )
+    mvtrow = select(mvtgeom, *fields.values()).filter(geom_filter).cte("mvtrow")
 
-    mvtquery = select(
-        ST_AsMVT(mvtrow.table_valued(), layer)
-    )
+    mvtquery = select(ST_AsMVT(mvtrow.table_valued(), layer))
 
     with Session(engine) as session:
         memview: Any = session.execute(mvtquery).scalar()
         result = bytes(memview) or None
 
     if result and cache_options:
-        base_path: Path = cache_options['base_path']
+        base_path: Path = cache_options["base_path"]
         tile_cache_path = base_path.joinpath(tile_path)
         tile_cache_path.parent.mkdir(parents=True, exist_ok=True)
 
-        with open(tile_cache_path, 'wb') as file:
+        with open(tile_cache_path, "wb") as file:
             file.write(result)
 
     return result or bytes()
